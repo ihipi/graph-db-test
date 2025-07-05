967c32c2900baf883c063d3f9d472e18a506e381
albert <albert.giro@gmail.com>
2025-07-05 20:19:17 +0200

    init

---
+++ graphdbtest/src/main/java/dev/agiro/demo/GraphdbtestApplication.java
@@ -0,0 +1,13 @@
+package dev.agiro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphdbtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphdbtestApplication.class, args);
+	}
+
+}
---
+++ graphdbtest/src/test/java/dev/agiro/demo/GraphdbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.agiro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphdbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
